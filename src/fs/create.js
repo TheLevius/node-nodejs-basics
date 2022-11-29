c04fa4e9c56fa82455c4620bad446a686b1277ee
reflevius <nikita.l@webspace.by>
2022-11-30 02:36:03 +0300

    feat: fs create.js implement function that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import {
+	writeFile
+} from 'node:fs/promises';
+import {
+	existsSync
+} from 'node:fs';
+import * as url from 'url';
+
 const create = async () => {
-    // Write your code here 
+
+	const __dirname = url.fileURLToPath(new URL('./files/',
+		import.meta.url));
+
+	const absolutePathFilename = __dirname + 'fresh.txt'
+
+	try {
+
+		if (existsSync(absolutePathFilename)) {
+			throw new Error('FS operation failed');
+		}
+
+		await writeFile(absolutePathFilename, 'I am fresh and young');
+
+	} catch (err) {
+
+		console.error(err);
+
+	}
 };
 
 await create();
