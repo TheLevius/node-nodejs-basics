4170a58e9113cb774cd1bee6783621de6ce56793
reflevius <nikita.l@webspace.by>
2022-11-30 15:02:58 +0300

    feat: fs copy.js implement function that copies folder files with all its content

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
+import * as url from 'url';
+import {
+	cp
+} from 'node:fs/promises';
+import {
+	existsSync
+} from 'node:fs';
+import {
+	stat
+} from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+
+	const srcUrl = url.fileURLToPath(new URL('./files',
+		import.meta.url))
+	const distUrl = url.fileURLToPath(new URL('./files_copy',
+		import.meta.url))
+
+	try {
+		const st = await stat(srcUrl)
+
+		if (!existsSync(srcUrl) || existsSync(distUrl)) {
+			throw new Error('FS operation failed');
+		}
+
+		await cp(srcUrl, distUrl, {
+			recursive: true
+		})
+
+	} catch (err) {
+
+		console.error(err)
+
+	}
+
 };
 
 copy();
