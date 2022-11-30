8a5daad8c5e5c87e141a06ee8b310a5636a6d94d
reflevius <nikita.l@webspace.by>
2022-11-30 17:10:25 +0300

    feat: fs list.js implement function that prints all array of filenames from files folder into console

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,29 @@
+import * as url from 'url'
+import {
+	readdir
+} from 'fs/promises';
+import {
+	existsSync
+} from 'node:fs';
+
 const list = async () => {
-    // Write your code here 
+	const listUrl = url.fileURLToPath(new URL('./files',
+		import.meta.url))
+
+	try {
+
+		if (!existsSync(listUrl)) {
+			throw new Error('FS operation failed');
+		}
+
+		const files = await readdir(listUrl);
+		files.forEach((file) => console.log(file))
+
+	} catch (err) {
+
+		console.error(err);
+
+	}
 };
 
 await list();
