764cb09094af15d29e44df6d00dbc2222339205c
reflevius <nikita.l@webspace.by>
2022-11-30 19:11:23 +0300

    feat: cli args.js implement function that parses command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+	const args = process.argv.slice(2)
+	const newArgs = []
+	for (let i = 0; i < args.length; i += 2) {
+
+		newArgs.push(`${args[i]} is ${args[i+1]}`)
+
+	}
+	console.log(newArgs.join(', '))
 };
 
 parseArgs();
