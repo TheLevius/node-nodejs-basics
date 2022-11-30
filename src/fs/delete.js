068a74a97d2f3ce7a234af171038389380985ffa
reflevius <nikita.l@webspace.by>
2022-11-30 16:59:43 +0300

    feat: fs delete.js implement function that deletes file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import * as url from 'url'
+import {
+	unlink
+} from 'fs/promises';
+import {
+	existsSync
+} from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+	const fileToRemove = url.fileURLToPath(new URL('./files/fileToRemove.txt',
+		import.meta.url))
+	try {
+		if (!existsSync(fileToRemove)) {
+			throw new Error('FS operation failed');
+		}
+		await unlink(fileToRemove)
+
+	} catch (err) {
+		console.error(err)
+	}
 };
 
 await remove();
