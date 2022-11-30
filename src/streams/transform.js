fb1681a2f748a9e6a2ea60870a2c5c7471a3bcc1
reflevius <nikita.l@webspace.by>
2022-12-01 02:07:47 +0300

    feat: streams implement transform stream function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import {
+	pipeline,
+	Transform
+} from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+
+	const reverser = new Transform({
+		transform(chunk, enc, cb) {
+			const chunkStringified = chunk.toString().trim();
+			const reversedData = chunkStringified.split('').reverse().join('');
+			cb(null, reversedData + '\n')
+		}
+	})
+
+	pipeline(
+		process.stdin,
+		reverser,
+		process.stdout,
+		(err) => {
+			console.error(`Error: ${err}`);
+		}
+	)
 };
 
 await transform();
