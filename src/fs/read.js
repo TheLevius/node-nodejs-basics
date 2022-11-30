a1b46158ecbd36b2bea39f68eec34d515de9fde0
reflevius <nikita.l@webspace.by>
2022-11-30 17:23:28 +0300

    feat: fs read.js implement function that prints content of the file into console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,32 @@
+import * as url from 'url'
+import {
+	readFile
+} from 'fs/promises';
+import {
+	existsSync
+} from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+
+	const fileUrl = url.fileURLToPath(new URL('./files/fileToRead.txt',
+		import.meta.url))
+
+	try {
+
+		if (!existsSync(fileUrl)) {
+			throw new Error('FS operation failed');
+		}
+
+		const data = await readFile(fileUrl, {
+			encoding: 'utf8'
+		});
+		console.log(data)
+
+	} catch (err) {
+
+		console.error(err);
+
+	}
 };
 
 await read();
