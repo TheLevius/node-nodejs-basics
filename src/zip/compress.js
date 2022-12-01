e89a0d690a938d79fa9dd9dbba98bcfc5f373d76
reflevius <nikita.l@webspace.by>
2022-12-01 10:19:01 +0300

    feat: zlib compress.js implement function that compresses file  to archive using zlib and Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import {
+	createReadStream,
+	createWriteStream
+} from 'node:fs';
+import {
+	createGzip
+} from 'node:zlib';
+import {
+	pipeline
+} from 'node:stream';
+import {
+	promisify
+} from 'node:util';
+
 const compress = async () => {
-    // Write your code here 
+	const pipe = promisify(pipeline);
+
+	const source = createReadStream('./files/fileToCompress.txt');
+	const gzip = createGzip();
+	const destination = createWriteStream('./files/archive.gz');
+
+	await pipe(
+		source,
+		gzip,
+		destination
+	)
 };
 
 await compress();
