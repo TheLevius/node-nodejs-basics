82b0ae6c11ff00aef20d73a4a0f3b171b997dcd0
reflevius <nikita.l@webspace.by>
2022-12-01 23:33:46 +0300

    feat: childProcesses cp.js implement function that receives array of arguments and creates child process from file from script.js passing these args to it

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import {
+	spawn
+} from 'node:child_process';
+
+const spawnChildProcess = async (args = []) => {
+	const childProcess = spawn('node', ['./files/script.js', ...args]);
+
+	process.stdin.on('data', (msg) => {
+		childProcess.stdin.write(msg)
+	})
+
+	childProcess.stdout.on('data', (data) => {
+		console.log(data.toString());
+	})
+
 };
 
 spawnChildProcess();
