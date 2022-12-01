188cd4fd18526d92d4cc27ccfde358953b8a161a
reflevius <nikita.l@webspace.by>
2022-12-01 18:07:55 +0300

    feat: workerThreads implement function that creates number of worker threads and able to send data to those threads and to receive result of the computation from them

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import {
+	Worker
+} from 'node:worker_threads';
+import {
+	cpus
+} from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+	const cores = cpus();
+	let num = 10;
+
+	const workers = cores.map(() => {
+		return new Promise((res, rej) => {
+			const worker = new Worker('./worker', {
+				workerData: num++
+			})
+			worker.on('message', (msg) => res(msg))
+			worker.on('error', (msg) => rej(msg))
+		})
+	})
+
+	const calcResults = await Promise.allSettled(workers)
+
+	const parsedResults = calcResults.map(({
+		status,
+		value
+	}) => status === 'fulfilled' ? {
+		status: 'resolved',
+		data: value.fbNum
+	} : {
+		status: 'error',
+		data: null
+	})
+	console.log(parsedResults)
 };
 
 await performCalculations();
